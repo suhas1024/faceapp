cc7757669c88261f4583af33390edd573ce137bc
suhas1024 <54569963+suhas1024@users.noreply.github.com>
2019-09-14 09:52:08 +0530

    rettina backend

--- recognize_faces_image.py
+++ recognize_faces_image.py
@@ -1,6 +1,7 @@
 # USAGE
 # python recognize_faces_image.py --encodings encodings.pickle --image examples/example_01.png 
 %matplotlib inline
+%config InlineBackend.figure_format = 'retina'
 #from matplotlib import pyplot as plt
 import matplotlib.pyplot as plt
 # import the necessary packages
